7243f7ae164155ae6486900722a26ba76d96856e
ltjuancr <juan_gonzalez93@hotmail.com>
2014-06-19 15:38:24 -0600

    Filtro 2 terminado

---
+++ MvcProyecto/MvcProyecto/Controllers/filterEmployeeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto.DataAccess;
+using MvcProyecto.Models;
+
+
+namespace MvcProyecto.Controllers
+{
+    public class filterEmployeeController : Controller
+    {
+        //
+        private SalePointEntities db = new SalePointEntities();// Nos ayuda a conectarnos a la db y esta linea se obtuvo del RestController
+
+        // GET: /filterClient/
+
+        public ActionResult Index()
+        {
+            // get data from db
+            var employees = db.EMPLOYEE.ToList();
+
+            // create model
+            var model = new filterEmployeeModels();
+
+            // fill restaurants in model
+            model.employee = employees;
+
+            // return model to the view
+            return View(model);
+        }
+
+        //public List<Restaurant> Restaurants { get; set; }
+
+
+        //
+        // POST: /Search/
+        [HttpPost]
+        public ActionResult Index(filterEmployeeModels model)
+        {
+            var search = model.SearchText;
+
+            if (string.IsNullOrEmpty(search))
+            {
+                model.employee = db.EMPLOYEE.ToList();
+            }
+            else
+            {
+                // fill restaurants in model
+                model.employee = db.EMPLOYEE.Where(x => x.postType1.description.Contains(search)).ToList();
+            }
+
+            // return model to the view
+            return View(model);
+        }
+
+    }
+}
---
+++ MvcProyecto/MvcProyecto/Models/filterEmployeeModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcProyecto.Models
+{
+        public class filterEmployeeModels
+        {
+            public string SearchText { get; set; }
+            public List<Proyecto.DataAccess.EMPLOYEE> employee { get; set; }
+        }
+}
